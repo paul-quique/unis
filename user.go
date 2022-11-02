66e6bec196ec7143630f1caed91c96c5d08ecf5a
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2022-11-02 16:02:19 +0100

    Update user.go

--- user.go
+++ user.go
@@ -5,8 +5,11 @@ import (
 	"strconv"
 	"time"
 
+	"net/mail"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
+	"golang.org/x/crypto/bcrypt"
 )
 
 const (
@@ -23,6 +26,13 @@ type User struct {
 	Password  string    `json:"password" db:"salted_hash"`
 	Points    int       `json:"points" db:"points"`
 	CreatedAt time.Time `json:"createdAt" db:"created_at"`
+}
+
+type CreateUserRequest struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email" `
+	Password  string `json:"password"`
 }
 
 func (u *User) CreateInDB(db *sqlx.DB) error {
@@ -73,6 +83,70 @@ func GetUser(c *gin.Context) {
 		"lastName":  u.LastName,
 		"email":     u.Email,
 		"points":    u.Points,
+		"createdAt": u.CreatedAt,
+	})
+}
+
+func PostUser(c *gin.Context) {
+	req := &CreateUserRequest{}
+	if err := c.BindJSON(req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "please provide valid user informations",
+		})
+		return
+	}
+
+	//vérifier que le nom et le prénom ne sont pas nuls
+	if req.FirstName == "" || req.LastName == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "please provide valid first name and last name",
+		})
+		return
+	}
+
+	//vérifier que l'email est de la forme d'une adresse mail
+	if _, err := mail.ParseAddress(req.Email); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "please provide a valid email address",
+		})
+		return
+	}
+
+	//vérifier que le mot de passe est assez long
+	if len(req.Password) < 8 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "password should be at least 8 characters long",
+		})
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "internal server error occured, please try again later",
+		})
+		return
+	}
+
+	u := &User{
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		Email:     req.Email,
+		Password:  string(hash),
+		CreatedAt: time.Now(),
+		Points:    100,
+	}
+
+	err = u.CreateInDB(APIDatabase)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "user may already exist, please login or try with another email",
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
 		"createdAt": u.CreatedAt,
 	})
 }
