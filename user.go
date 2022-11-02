629f34798cf5cb92ef93ed7df592adf9c7a4ab6a
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2022-11-02 16:13:43 +0100

    Update user.go

--- user.go
+++ user.go
@@ -29,8 +29,8 @@ type User struct {
 }
 
 type CreateUserRequest struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
 	Email     string `json:"email" `
 	Password  string `json:"password"`
 }
