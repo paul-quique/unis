439a1f92d172e84b04bbfa8a7ff76f8182dab7d1
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2022-11-15 11:42:35 +0100

    Update profile_pic_generator.go

---
+++ profile_pic_generator.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"math/rand"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomImage() *image.RGBA {
+	r, g, b := Uint8n(256), Uint8n(256), Uint8n(256)
+	c := color.NRGBA{r, g, b, 255}
+	i := image.NewRGBA(image.Rect(0, 0, 500, 500))
+	draw.Draw(i, i.Bounds(), &image.Uniform{c}, image.ZP, draw.Src)
+	return i
+}
+
+func GenerateProfilePicture(c *gin.Context) {
+	err := png.Encode(c.Writer, RandomImage())
+	if err != nil {
+		panic(err)
+	}
+}
+func Uint8n(n int) uint8 {
+	return uint8(rand.Intn(n))
+}
