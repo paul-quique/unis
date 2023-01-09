b2a1479e9880fe5ae80152011bc1f7c4b1d9e0bb
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2023-01-09 09:14:42 +0100

    j
    h

--- session.go
+++ session.go
@@ -11,13 +11,8 @@ import (
 )
 
 const (
-<<<<<<< Updated upstream
 	INSERT_SESSION         = "INSERT INTO session (id, user_id, expires_at) VALUES (:id, :user_id, NOW() + interval '60 minutes');"
 	GET_USER_BY_SESSION_ID = "SELECT user_info.* FROM session JOIN user_info ON session.user_id = user_info.id WHERE session.id=$1 AND expires_at >= now();"
-=======
-	INSERT_SESSION         = "INSERT INTO session (id, user_id, expires_at) VALUES (:id, :user_id, :expires_at);"
-	GET_USER_BY_SESSION_ID = "SELECT user_info.* FROM session JOIN user_info ON session.user_id = user_info.id WHERE session.id=$1 AND expires_at <= now();"
->>>>>>> Stashed changes
 )
 
 type Session struct {
@@ -33,14 +28,8 @@ type LoginRequest struct {
 
 func NewSessionFromUser(u *User) *Session {
 	return &Session{
-<<<<<<< Updated upstream
 		Id:     uuid.New().String(),
 		UserId: u.Id,
-=======
-		Id:        uuid.New().String(),
-		UserId:    u.Id,
-		ExpiresAt: time.Now().Add(60 * time.Second),
->>>>>>> Stashed changes
 	}
 }
 
@@ -91,11 +80,7 @@ func Auth(c *gin.Context) {
 		})
 		return
 	}
-<<<<<<< Updated upstream
 	c.JSON(http.StatusCreated, gin.H{
 		"sessID": s.Id,
 	})
-=======
-	c.JSON(http.StatusCreated, s)
->>>>>>> Stashed changes
 }
