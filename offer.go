614f9536c540694bd941a1221ca1eec842b8691a
Paul Quique <48380381+paul-quique@users.noreply.github.com>
2023-02-24 21:08:37 +0100

    Update offer.go

--- offer.go
+++ offer.go
@@ -34,7 +34,12 @@ type Transaction struct {
 }
 
 type AcceptedOffer struct {
-	Offer      Offer
+	Id         int       `json:"id" db:"id"`
+	BorrowerId int       `json:"borrowerId" db:"borrower_id"`
+	LenderId   int       `json:"lenderId" db:"lender_id"`
+	ProductId  int       `json:"productId" db:"product_id"`
+	CreatedAt  time.Time `json:"createdAt" db:"created_at"`
+	ExpiresAt  time.Time `json:"expiresAt" db:"expires_at"`
 	ReturnDate time.Time `json:"returnDate" db:"return_date"`
 }
 
